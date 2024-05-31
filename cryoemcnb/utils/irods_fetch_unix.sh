046d4a1131e4bffe1e91a387b7915524dad6952a
sanlopez <irene.sanchezl93@gmail.com>
2024-05-31 12:47:56 +0200

    changed CEITEC github URLs for this repo URLs

--- cryoemcnb/utils/irods_fetch_unix.sh
+++ cryoemcnb/utils/irods_fetch_unix.sh
@@ -1,6 +1,6 @@
 # The following code was from https://github.com/cemcof/cemcof.github.io/blob/main/irods_fetch_unix.sh
 
-# Invoke: curl -sSfL "https://raw.githubusercontent.com/cemcof/cemcof.github.io/main/irods_fetch_unix.sh" | bash -s -- --host "{host}" -t "{ticket}" -c "{colleciton_path}"
+# Invoke: curl -sSfL "https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py" | bash -s -- --host "{host}" -t "{ticket}" -c "{colleciton_path}"
 
 # Ensure irods module is installed
 if python3 -c "import irods" >/dev/null 2>&1; then
@@ -12,7 +12,7 @@ fi
 
 
 # Execute downloader from url
-python_script_url="https://raw.githubusercontent.com/cemcof/cemcof.github.io/main/irods_fetch.py"
+python_script_url="https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py"
 curl -fL $python_script_url | python3 - $@
 
 # Alternative:
