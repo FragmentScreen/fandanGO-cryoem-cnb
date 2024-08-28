bea215db9ae54706bd57001c58748928e78ed5d2
sanlopez <irene.sanchezl93@gmail.com>
2024-08-28 13:22:40 +0200

    iRODS download script more robust

--- cryoemcnb/utils/irods_fetch.py
+++ cryoemcnb/utils/irods_fetch.py
@@ -1,6 +1,6 @@
-# The following code was from https://github.com/cemcof/cemcof.github.io/blob/main/irods_fetch.py
+# The following code was taken from https://github.com/cemcof/cemcof.github.io/blob/main/irods_fetch.py
 
-# iRODS collection downloader 
+# iRODS collection downloader
 
 import argparse, pathlib
 
@@ -38,70 +38,80 @@ arguments = aparser.parse_args()
 
 # Extract zone from the collection argument (first path part)
 arguments.zone = arguments.collection_path.parts[1]
-print(arguments.zone)
 
-# Open iRODS session
-with iRODSSession(port=arguments.port, host=arguments.host, user=arguments.user, password=arguments.password, zone=arguments.zone) as irods_session:
-    
-    # -------- grent ticket
-    # new_t = Ticket(irods_session)
-    # new_t.issue("read", arguments.collection_path)
-    # print(new_t.string)
-    # exit()
-    # -------
+def download_completed(port, host, user, password, zone):
+    # Open iRODS session
+    with iRODSSession(port=port, host=host, user=user, password=password, zone=zone) as irods_session:
 
-    # Supply the access ticket, if any 
-    if arguments.ticket:
-        Ticket(irods_session, arguments.ticket).supply()
-        # collection = iRODSCollection(irods_session.collections, irods_session.query(Collection).one())
-    #else:
-    #    collection = irods_session.collections.get(arguments.collection_path)
+        # -------- grent ticket
+        # new_t = Ticket(irods_session)
+        # new_t.issue("read", arguments.collection_path)
+        # print(new_t.string)
+        # exit()
+        # -------
 
-    
-    #irods_session.query()
-    collection = irods_session.collections.get(str(arguments.collection_path))
-    print(f"Connected to iRODS, starting file downloads from the collection {collection.path}...")
+        # Supply the access ticket, if any
+        if arguments.ticket:
+            Ticket(irods_session, arguments.ticket).supply()
+            # collection = iRODSCollection(irods_session.collections, irods_session.query(Collection).one())
+        #else:
+        #    collection = irods_session.collections.get(arguments.collection_path)
 
-    def walk_collection(collection: iRODSCollection):
-        for subcol in collection.subcollections:
-            yield from walk_collection(subcol) # Recurse into subcollections
-        
-        for dobj in collection.data_objects:
-            yield dobj
 
-    data_objects = list(walk_collection(collection))
-    total_files = len(data_objects)
-    current_file = 0
+        #irods_session.query()
+        collection = irods_session.collections.get(str(arguments.collection_path))
+        print(f"Connected to iRODS, starting file downloads from the collection {collection.path}...")
 
-    # Download collection files
-    for data_obj in data_objects:
-        target_path: pathlib.Path = arguments.output_dir / pathlib.Path(data_obj.path).relative_to(arguments.collection_path) 
+        def walk_collection(collection: iRODSCollection):
+            for subcol in collection.subcollections:
+                yield from walk_collection(subcol) # Recurse into subcollections
 
-        # Check if file already exists and has same size as collection file
-        # In that case, skip it
-        if target_path.exists():
-            # File already there - does it have the same size?
-            if target_path.stat().st_size == data_obj.size:
-                # Skip - it is already downloaded
-                print(f"[{current_file+1}/{total_files}] Skipped file {target_path} - already downloaded")
+            for dobj in collection.data_objects:
+                yield dobj
+
+        data_objects = list(walk_collection(collection))
+        total_files = len(data_objects)
+        current_file = 0
+
+        try:
+            # Download collection files
+            for data_obj in data_objects:
+                target_path: pathlib.Path = arguments.output_dir / pathlib.Path(data_obj.path).relative_to(arguments.collection_path)
+
+                # Check if file already exists and has same size as collection file
+                # In that case, skip it
+                if target_path.exists():
+                    # File already there - does it have the same size?
+                    if target_path.stat().st_size == data_obj.size:
+                        # Skip - it is already downloaded
+                        print(f"[{current_file+1}/{total_files}] Skipped file {target_path} - already downloaded")
+                        current_file = current_file + 1
+                        continue
+                    else:
+                        target_path.unlink(missing_ok=True)
+
+                # Ensure directory exists for the target file
+                if not target_path.parent.is_dir():
+                    target_path.parent.mkdir(parents=True, exist_ok=True)
+
+                # Download the file
+                print(f"[{current_file+1}/{total_files}] Downloading file {str(target_path)}...", end='', flush=True)
+                start = time.time()
+                irods_session.data_objects.get(data_obj.path, str(target_path))
+                duration_sec = time.time() - start
+                size = sizeof_fmt(target_path.stat().st_size)
+                print(f" done, {duration_sec:.2f} sec, {size}")
                 current_file = current_file + 1
-                continue
-            else: 
-                target_path.unlink(missing_ok=True)
-        
-        # Ensure directory exists for the target file
-        if not target_path.parent.is_dir():
-            target_path.parent.mkdir(parents=True, exist_ok=True)
 
-        # Download the file
-        print(f"[{current_file+1}/{total_files}] Downloading file {str(target_path)}...", end='', flush=True)
-        start = time.time()
-        irods_session.data_objects.get(data_obj.path, str(target_path))
-        duration_sec = time.time() - start
-        size = sizeof_fmt(target_path.stat().st_size)
-        print(f" done, {duration_sec:.2f} sec, {size}")
-        current_file = current_file + 1
+        except Exception as e:
+            print(f"Something bad happened while trying to get {data_obj.path}: {e}")
+
+    return total_files == current_file
+
+while True:
+    is_complete = download_completed(arguments.port, arguments.host, arguments.user, arguments.password, arguments.zone)
+    if is_complete:
+        break
 
 # iRODS session closed
-
 print(f"Successfully downloaded collection: {arguments.collection_path}")
