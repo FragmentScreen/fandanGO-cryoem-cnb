1a31446071d0f51386fc260a95d62280a0dd160c
sanlopez <irene.sanchezl93@gmail.com>
2025-04-23 11:24:43 +0200

    use python or python3

--- cryoemcnb/utils/irods_fetch_unix.sh
+++ cryoemcnb/utils/irods_fetch_unix.sh
@@ -2,33 +2,43 @@
 
 # Invoke: curl -sSfL "https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch_unix.sh" | bash -s -- --host "{host}" --ticket "{ticket}" --collection "{colleciton_path}"
 
+# --- Find compatible Python executable ---
+if command -v python3 &>/dev/null; then
+    PYTHON=python3
+elif command -v python &>/dev/null; then
+    PYTHON=python
+else
+    echo "❌ Python is not installed."
+    exit 1
+fi
+
 # Ensure irods module is installed
 version_ge() {
     printf '%s\n%s\n' "$2" "$1" | sort -V | head -n 1 | grep -qx "$2"
 }
 
-if python -c "import irods; print(irods.__version__)" >/dev/null 2>&1; then
-    VERSION=$(python -c "import irods; print(irods.__version__)")
+if $PYTHON -c "import irods; print(irods.__version__)" >/dev/null 2>&1; then
+    VERSION=$($PYTHON -c "import irods; print(irods.__version__)")
     REQUIRED_VERSION="3.0.0"
 
     if version_ge "$VERSION" "$REQUIRED_VERSION"; then
         echo "iRODS module found (version $VERSION)."
     else
         echo "iRODS version $VERSION found, but at least $REQUIRED_VERSION is required. Upgrading..."
-        python -m pip install --upgrade "python-irodsclient>=$REQUIRED_VERSION"
+        $PYTHON -m pip install --upgrade "python-irodsclient>=$REQUIRED_VERSION"
     fi
 else
     echo "python-irodsclient is not installed. Installing..."
-    python -m pip install "python-irodsclient>=3.0.0"
+    $PYTHON -m pip install "python-irodsclient>=3.0.0"
 fi
 
 
 # Execute downloader from url
 python_script_url="https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py"
-curl -fL $python_script_url | python - $@
+curl -fL $python_script_url | $PYTHON - $@
 
 # Alternative:
 # temp_file=$(mktemp)
 # curl -sSfL -o "$temp_file" "$python_script_url"
-# python "$temp_file" $@
+# $PYTHON "$temp_file" $@
 # rm "$temp_file"
