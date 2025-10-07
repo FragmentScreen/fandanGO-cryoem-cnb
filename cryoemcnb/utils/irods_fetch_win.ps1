19b668c351d3bc426445c8507d56eee5e819b8e4
sanlopez <irene.sanchezl93@gmail.com>
2025-10-07 12:41:41 +0200

    ask for user confirmation before installing python-irodsclient module

--- cryoemcnb/utils/irods_fetch_win.ps1
+++ cryoemcnb/utils/irods_fetch_win.ps1
@@ -1,41 +1,52 @@
-# Invoke: $scriptPath = "$(Get-Location)\irods_fetch_win.ps1"; (Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/refs/heads/main/cryoemcnb/utils/irods_fetch_win.ps1").Content | Out-File $scriptPath -Encoding UTF8; & powershell -ExecutionPolicy Bypass -File $scriptPath --host "{host}" --ticket "{ticket}" --collection "{colleciton_path}"; Remove-Item $scriptPath
+# Invoke: $scriptPath = "$(Get-Location)\irods_fetch_win.ps1"; (Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/refs/heads/main/cryoemcnb/utils/irods_fetch_win.ps1").Content | Out-File $scriptPath -Encoding UTF8; & powershell -ExecutionPolicy Bypass -File $scriptPath --host "{host}" --ticket "{ticket}" --collection "{collection_path}" --output_dir "{output_dir}"; Remove-Item $scriptPath
 
 # Define path to Python
 $PYTHON = "python"
 
-# Ensure irods module is installed
-function Version-Ge {
-    param (
-        [string]$version1,
-        [string]$version2
-    )
-    return ([Version]$version1 -ge [Version]$version2)
-}
+$REQUIRED_VERSION = "3.0.0"
+DOWNLOAD=$false
 
-# Intentar obtener la versión del módulo iRODS
+# Ensure irods module is installed
+
 try {
     $VERSION = & $PYTHON -c "import irods; print(irods.__version__)" 2>$null
 } catch {
     $VERSION = $null
 }
 
-$REQUIRED_VERSION = "3.0.0"
-
 if ($VERSION) {
     $VERSION = $VERSION.Trim()
-    if (Version-Ge $VERSION $REQUIRED_VERSION) {
+    if ($VERSION -eq $REQUIRED_VERSION) {
         Write-Host "iRODS module found (version $VERSION)."
+        DOWNLOAD=$true
     } else {
-        Write-Host "iRODS version $VERSION found, but at least $REQUIRED_VERSION is required. Upgrading..."
-        & $PYTHON -m pip install --upgrade "python-irodsclient>=$REQUIRED_VERSION"
+        $UPGRADE = Read-Host "iRODS version $VERSION found, but $REQUIRED_VERSION is required. Do you want to change to it? [y/n]"
+        $UPGRADE = $UPGRADE.ToLower()
+        if ($UPGRADE -eq "y" -or $UPGRADE -eq "yes") {
+            Write-Host "Changing python-irodsclient version..."
+            & $PYTHON -m pip install --upgrade "python-irodsclient==$REQUIRED_VERSION"
+            $DOWNLOAD = $true
+        } else {
+            Write-Host "Not changing python-irodsclient version"
+        }
     }
 } else {
-    Write-Host "python-irodsclient is not installed. Installing..."
-    & $PYTHON -m pip install "python-irodsclient>=3.0.0"
+    $INSTALL = Read-Host "python-irodsclient is not installed or could not get the version. Do you want to install it (version $REQUIRED_VERSION)? [y/n]"
+    $INSTALL = $INSTALL.ToLower()
+    if ($INSTALL -eq "y" -or $INSTALL -eq "yes") {
+        Write-Host "Installing python-irodsclient..."
+        & $PYTHON -m pip install "python-irodsclient==$REQUIRED_VERSION"
+        $DOWNLOAD = $true
+        } else {
+            Write-Host "Not changing python-irodsclient version"
+        }
 }
 
-
-# Execute downloader from url
-$python_script_url="https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py"
-$scriptContent = Invoke-WebRequest -Uri $python_script_url -UseBasicParsing | Select-Object -ExpandProperty Content
-$scriptContent | & $PYTHON - @args
+if ($DOWNLOAD) {
+    # Execute downloader from url
+    $python_script_url="https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py"
+    $scriptContent = Invoke-WebRequest -Uri $python_script_url -UseBasicParsing | Select-Object -ExpandProperty Content
+    $scriptContent | & $PYTHON - @args
+} else {
+    Write-Host "No data will be downloaded."
+}
