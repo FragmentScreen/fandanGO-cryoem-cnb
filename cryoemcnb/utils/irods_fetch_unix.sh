beddf211c3fae3da2860ad80bd7d025743257b81
sanlopez <irene.sanchezl93@gmail.com>
2025-10-07 12:11:01 +0200

    ask for user confirmation before installing python-irodsclient module

--- cryoemcnb/utils/irods_fetch_unix.sh
+++ cryoemcnb/utils/irods_fetch_unix.sh
@@ -1,31 +1,49 @@
 # The following code was from https://github.com/cemcof/cemcof.github.io/blob/main/irods_fetch_unix.sh
 
-# Invoke: curl -sSfL "https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch_unix.sh" | bash -s -- --host "{host}" --ticket "{ticket}" --collection "{colleciton_path}"
+# Invoke: curl -sSfL "https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch_unix.sh" | bash -s -- --host "{host}" --ticket "{ticket}" --collection "{colleciton_path}" --output_dir "{output_dir}"
 
 # Define path to Python
 PYTHON="python3"
 
+REQUIRED_VERSION="3.0.0"
+download=false
+
 # Ensure irods module is installed
-version_ge() {
-    printf '%s\n%s\n' "$2" "$1" | sort -V | head -n 1 | grep -qx "$2"
-}
 
 if $PYTHON -c "import irods; print(irods.__version__)" >/dev/null 2>&1; then
     VERSION=$($PYTHON -c "import irods; print(irods.__version__)")
-    REQUIRED_VERSION="3.0.0"
 
-    if version_ge "$VERSION" "$REQUIRED_VERSION"; then
+    if [ "$VERSION" = "$REQUIRED_VERSION" ]; then
         echo "iRODS module found (version $VERSION)."
+        download=true
+
     else
-        echo "iRODS version $VERSION found, but at least $REQUIRED_VERSION is required. Upgrading..."
-        $PYTHON -m pip install --upgrade "python-irodsclient>=$REQUIRED_VERSION"
+        read -p "iRODS version $VERSION found, but $REQUIRED_VERSION is required. Do you want to change to it? [y/n]" UPGRADE
+        UPGRADE=${UPGRADE,,}  # to lowercase
+        if [[ "$UPGRADE" == "y" || "$UPGRADE" == "yes" ]]; then
+            echo "Changing python-irodsclient version..."
+            $PYTHON -m pip install --upgrade "python-irodsclient==$REQUIRED_VERSION"
+            download=true
+        else
+            echo "Not changing python-irodsclient version"
+        fi
     fi
 else
-    echo "python-irodsclient is not installed. Installing..."
-    $PYTHON -m pip install "python-irodsclient>=3.0.0"
+    read -p "python-irodsclient is not installed or could not get the version. Do you want to install it (version $REQUIRED_VERSION)? [y/n]" INSTALL
+    INSTALL=${INSTALL,,}  # to lowercase
+    if [[ "$INSTALL" == "y" || "$INSTALL" == "yes" ]]; then
+        echo "Installing python-irodsclient..."
+        $PYTHON -m pip install "python-irodsclient==$REQUIRED_VERSION"
+        download=true
+    else
+        echo "Not installing python-irodsclient"
+    fi
 fi
 
-
-# Execute downloader from url
-python_script_url="https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py"
-curl -fL $python_script_url | $PYTHON - $@
+if [ "$download" = true ]; then
+    # Execute downloader from url
+    python_script_url="https://raw.githubusercontent.com/FragmentScreen/fandanGO-cryoem-cnb/main/cryoemcnb/utils/irods_fetch.py"
+    curl -fL $python_script_url | $PYTHON - $@
+else
+    echo "No data will be downloaded."
+fi
