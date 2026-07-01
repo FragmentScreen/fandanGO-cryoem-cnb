81867c8d83c6d37a1bbce8f3a151d4e4b38ffabc
alberto <alberto.garcia@cnb.csic.es>
2026-07-01 10:46:59 +0200

    Fix key check for raw_data_ticket_windows
    prject_info provide just two values not three

--- cryoemcnb/actions/provide_raw_data_windows.py
+++ cryoemcnb/actions/provide_raw_data_windows.py
@@ -9,7 +9,7 @@ def provide_raw_data(project_name, raw_data_links):
     if raw_data_links:
         success = True
         column_names, project_info = get_project_info(project_name)
-        exists = any(key == "raw_data_ticket_windows" for _, key, _ in project_info)
+        exists = any(key == "raw_data_ticket_windows" for key, _ in project_info)
         print('exist a raw_data_ticket_windows on the project, associating the new...') if exists else print('raw_data_ticket_windows does not exist on the project, associating...')
         update_project(project_name, 'raw_data_ticket_windows', raw_data_links)
         info = {'raw_data_ticket_windows': raw_data_links}
